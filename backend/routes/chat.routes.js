ba716f7d48b41df967d72c74aa9f38ba87ed0c6c
alanjames00 <alanjames6282@gmail.com>
2024-04-24 01:49:04 +0530

    complete create chat routes

--- backend/routes/chat.routes.js
+++ backend/routes/chat.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const jwt = require('../utils/jwt')
 
 // router setup
 const chatRouter = express.Router();
@@ -10,9 +10,9 @@ const redis = require('./redis');
 
 // test endpoints
 chatRouter.get('/t', (req, res) => {
-  res.json({
-    info : 'hit chat/t'
-  })
+	res.json({
+		info: 'hit chat/t'
+	})
 });
 
 // create a new chat
@@ -20,27 +20,63 @@ chatRouter.get('/t', (req, res) => {
 // create a chat
 chatRouter.post('/createChat', async (req, res) => {
 
-  try {
+	try {
 
-    // check if the same chat grp with given name exist or not
-    const { roomName, password, isPasswordProtected } = req.body;
+		// check if the same chat grp with given name exist or not
+		const { roomName, username } = req.body;
 
-    const redisKey = `chatroom:${roomName}`
-    const redisResult = await redis.get(redisKey)
-    
-    if(redisResult == null) {
-      console.log('room does not exist');
-      // create the room
-    }
-    else {
-      console.log('room exists');
-    }
-    
-  }
-  catch(e) {
-    console.log(e);
-    res.status(500).json(e);
-  }
+		// check for roomname and username 
+		if (roomName == null || username == null) {
+			res.status(400).json({
+				error: 'roomName or username not provided'
+			});
+			return;
+		}
+
+		const redisKey = `chatroom:${roomName}:members`
+		const redisResult = await redis.exists(redisKey)
+		console.log(redisResult);
+		if (redisResult == null) {
+
+			// create a room set with the username
+			await redis.sadd(`chatroom:${roomName}:members`, username);
+
+			// now generate token
+			const token = jwt.generateToken({ roomName, username });
+
+			res.json({
+				token
+			})
+		}
+		else {
+
+			// check if the user is already in the room
+			const isMember = await redis.sismember(`chatroom:${roomName}:members`, username);
+			if (isMember == 1) {
+				res.status(400).json({
+					err: 'user_exist'
+				});
+			}
+
+			else {
+				// add user to the room
+				await redis.sadd(`chatroom:${roomName}:members`, username);
+
+				// now generate token
+				const token = jwt.generateToken({ roomName, username });
+
+				res.json({
+					token
+				})
+
+			}
+
+		}
+	}
+	catch (e) {
+		console.log(e);
+		res.status(500).json(e);
+	}
 })
 
 
