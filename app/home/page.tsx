32907c58b490061bfca011375e6033631dd132c5
alanjames00 <alanjames6282@gmail.com>
2024-04-24 02:20:16 +0530

    add ws connector and states

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,41 @@
+'use client'
 import MessageContainer from "../components/messages/MessageContainer";
 import Sidebar from "../components/sidebar/Sidebar";
+import { useState, useEffect, use } from 'react';
 
 const Home = () => {
+
+	const [ws, setWs] = useState<any>(null);
+
+	// check if the user has the token
+	
+	useEffect(() => {
+		const token = localStorage.getItem("chat-user-token");
+		if (!token) {
+			window.location.href = "/login";
+		}
+		console.log("token", token);
+
+		// connect to the websocket server
+		const socket = new WebSocket("ws://localhost:8080");
+		socket.onopen = () => {
+			console.log("connected to the server");
+		};
+
+		
+
+		socket.onmessage = (message) => {
+			console.log("message", message);
+		};
+		
+
+		// set the state
+		setWs(socket);
+	}, []);
+
+	console.log("ws state", ws);
+
+
 	return (
 		
 		<div className='flex h-full w-screen justify-center items-cen msger-chat'>
